362822905bf60bb38a28c0290a1150ef65a4dd79
anthonydo8196 <anthonydo8196@gmail.com>
2024-11-02 02:58:53 +0000

    Completed User Register

--- src/main/java/Controller/SocialMediaController.java
+++ src/main/java/Controller/SocialMediaController.java
@@ -1,7 +1,12 @@
 package Controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import Model.Account;
 import io.javalin.Javalin;
 import io.javalin.http.Context;
+import Service.AccountService;
 
 /**
  * TODO: You will need to write your own endpoints and handlers for your controller. The endpoints you will need can be
@@ -9,6 +14,12 @@ import io.javalin.http.Context;
  * refer to prior mini-project labs and lecture materials for guidance on how a controller may be built.
  */
 public class SocialMediaController {
+    AccountService accountService;
+
+    public SocialMediaController() {
+        this.accountService = new AccountService();
+    }
+
     /**
      * In order for the test cases to work, you will need to write the endpoints in the startAPI() method, as the test
      * suite must receive a Javalin object from this method.
@@ -16,8 +27,22 @@ public class SocialMediaController {
      */
     public Javalin startAPI() {
         Javalin app = Javalin.create();
-        app.get("example-endpoint", this::exampleHandler);
+        app.get("/ex", this::exampleHandler);
+        
+        // POST register
+        app.post("/register", this::postRegisterHandler);
 
+        // POST login
+
+        // POST messages
+
+        // GET messages/{message_id}
+
+        // DELETE messages/{message_id}
+
+        // PATCH messages/{message_id}
+
+        // GET accounts/{account_id}/messages
         return app;
     }
 
@@ -25,9 +50,20 @@ public class SocialMediaController {
      * This is an example handler for an example endpoint.
      * @param context The Javalin Context object manages information about both the HTTP request and response.
      */
-    private void exampleHandler(Context context) {
-        context.json("sample text");
+    private void exampleHandler(Context ctx) {
+        ctx.json("example handler!!\n");
     }
 
+    // Handler to register a new account
+    private void postRegisterHandler(Context ctx) throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
+        Account acc = mapper.readValue(ctx.body(), Account.class);
+        Account addedAcc = accountService.addAccount(acc);
+        if (addedAcc != null) {
+            ctx.json(mapper.writeValueAsString(addedAcc));
+        } else { 
+            ctx.status(400);
+        }   
+    }
 
 }
---
+++ src/main/java/DAO/AccountDAO.java
@@ -0,0 +1,74 @@
+package DAO;
+
+import Model.Account;
+import Util.ConnectionUtil;
+
+import java.sql.*;
+import java.util.List;
+import java.util.ArrayList;
+
+public class AccountDAO {
+
+    public List<Account> getAllAccounts() {
+        Connection connection = ConnectionUtil.getConnection();
+        List<Account> accs = new ArrayList<>();
+        try {
+            String sql = "SELECT * FROM account";
+            PreparedStatement ps = connection.prepareStatement(sql);
+            ResultSet rs = ps.executeQuery();
+            while(rs.next()) {
+                Account account = new Account(rs.getInt("account_id"), rs.getString("username"), rs.getString("password"));
+                accs.add(account);
+            }
+        } catch(SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return accs;
+    }
+
+    public Account insertAccount(Account account) {
+        // Connection object configured to connect to our specific database
+        Connection connection = ConnectionUtil.getConnection();
+        try {
+            // make sure username isn't blank, password is at least 4 characters long
+            if(account.getUsername().isEmpty() || account.getPassword().length() < 4) {
+                return null;
+            } 
+
+            // check that account with this username doesn't already exist
+            String sql = "SELECT * FROM account WHERE username = ?";
+            PreparedStatement ps = connection.prepareStatement(sql);
+            ps.setString(1, account.getUsername());
+            ResultSet rs = ps.executeQuery();
+
+            // result set.next returns true if it can move to an available row otherwise it returns false
+            if(rs.next()) {
+                return null;
+            }
+                
+
+            // otherwise continue normally
+            // prep sql statement and return any generated keys (account_id in this case)
+            sql = "INSERT INTO account (username, password) VALUES (?, ?)" ;
+            ps = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+
+            // set the values for the placeholders
+            ps.setString(1, account.getUsername());
+            ps.setString(2, account.getPassword());
+
+            // execute the insert statement
+            ps.executeUpdate();
+            // get the generated keys
+            rs = ps.getGeneratedKeys();
+            // moves cursor to first row of result set and returns true if there's a row
+            if(rs.next()){
+                int generated_account_id = (int) rs.getLong(1);
+                // return account object with it's proper fields
+                return new Account(generated_account_id, account.getUsername(), account.getPassword());
+            }
+        } catch(SQLException e){
+            System.out.println(e.getMessage());
+        }
+        return null;
+    }    
+}
